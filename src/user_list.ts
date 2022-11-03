a583e07074881fe1a14dfb03e658e77cde561fb4
Daniel Monjas <dmonjasm@gmail.com>
2022-11-03 18:00:22 +0100

    Avanzo #7, pues me había olvidado el constructor

--- src/user_list.ts
+++ src/user_list.ts
@@ -1,5 +1,9 @@
 import { Anime } from "./anime"
 
 class UserList{
-    animes_usuario: Anime[];
+    animes_user: Anime[];
+
+    constructor(animes_user:Anime[]){
+        this.animes_user = animes_user
+    }
 }

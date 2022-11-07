c9cd8a6b8b8d45f8df34a003e2105ac6308e915f
vdeq79 <xszh79@outlook.com>
2022-11-07 17:53:18 +0100

    Actualizo Gruntfile con el orden check

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,12 @@
 module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-shell');
+    grunt.initConfig({
+        shell:{
+            check:{
+                command: 'deno check --reload ./src/*.ts'
+            }
+        }
+    });
 
+    grunt.registerTask('check', 'check code' , ['shell']);
 };

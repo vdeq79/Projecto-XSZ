6930523e704d521f1efe54d766df2cab7c99c3c4
Daniel Monjas <dmonjasm@gmail.com>
2022-11-21 22:30:35 +0100

    Añadidas creación de variables a user_list.ts y anime.ts. Corregido error en import de user_list.ts

--- src/anime.ts
+++ src/anime.ts
@@ -28,3 +28,13 @@ export class Anime{
         this.broadcast = broadcast; //Día de la semana que se lanza. 
     }
 }
+
+let anime = new Anime({'English': 'Demon Slayer'},
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. \
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                        12,
+                        "TV",
+                        "Finished",
+                        ["Action"],
+                        2001,
+                        new Date(2018, 0O5, 0O5, 17, 23, 42, 11))
--- src/user_list.ts
+++ src/user_list.ts
@@ -1,4 +1,4 @@
-import { Anime } from "./anime"
+import { Anime } from "./anime.ts"
 
 class UserList{
     animesUser: Anime[];
@@ -7,3 +7,15 @@ class UserList{
         this.animesUser = animesUser
     }
 }
+
+let anime = new Anime({'English': 'Demon Slayer'},
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. \
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                        12,
+                        "TV",
+                        "Finished",
+                        ["Action"],
+                        2001,
+                        new Date(2018, 0O5, 0O5, 17, 23, 42, 11))
+
+let list = new UserList([anime])

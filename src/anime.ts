1ad83424053bb42611a755501c627755c2f97b82
Daniel Monjas <dmonjasm@gmail.com>
2022-11-23 11:55:52 +0100

    Eliminado synopsis y broadcast como atributos de la clase Anime conforme a #14

--- src/anime.ts
+++ src/anime.ts
@@ -10,31 +10,24 @@ export class Anime{
 
     constructor(
         titles: {[language:string]: string},
-        synopsis: string,
         episodes: number,
         type: string,
         status: string,
         genres: string[],
         year: number,
-        broadcast: Date
     ){
         this.titles = titles; //Títulos en distintos idiomas
-        this.synopsis = synopsis; //Sinopsis
         this.episodes = episodes; //Num episodios
         this.type = type; //TV, OVA, Film, ...
         this.status = status //En emisión, finalizado, etc.
         this.genres = genres; //Action, Fantasy, etc
         this.year = year; // Año de lanzamiento
-        this.broadcast = broadcast; //Día de la semana que se lanza. 
     }
 }
 
 let anime = new Anime({'English': 'Demon Slayer'},
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. \
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
                         12,
                         "TV",
                         "Finished",
                         ["Action"],
-                        2001,
-                        new Date(2018, 0O5, 0O5, 17, 23, 42, 11))
+                        2001)
--- src/user_list.ts
+++ src/user_list.ts
@@ -9,13 +9,10 @@ class UserList{
 }
 
 let anime = new Anime({'English': 'Demon Slayer'},
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. \
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
                         12,
                         "TV",
                         "Finished",
                         ["Action"],
-                        2001,
-                        new Date(2018, 0O5, 0O5, 17, 23, 42, 11))
+                        2001)
 
 let list = new UserList([anime])

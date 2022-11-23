c509c2cbf07cc54ebb5d211e5b58dc9b71e09fef
Daniel Monjas <dmonjasm@gmail.com>
2022-11-23 19:27:23 +0100

    Eliminados ejemplos del código. Corrigiendo #14

--- src/anime.ts
+++ src/anime.ts
@@ -35,12 +35,3 @@ export class Anime{
         this.studios = studios // Estudio animador
     }
 }
-
-let anime = new Anime({'English': 'Demon Slayer'},
-                        12,
-                        "TV",
-                        "Finished",
-                        ["Action"],
-                        2001,
-                        Status.ended,
-                        ["Mappa"])
--- src/user_list.ts
+++ src/user_list.ts
@@ -7,14 +7,3 @@ class UserList{
         this.animesUser = animesUser
     }
 }
-
-let anime = new Anime({'English': 'Demon Slayer'},
-                        12,
-                        "TV",
-                        "Finished",
-                        ["Action"],
-                        2001,
-                        Status.ended,
-                        ["Mappa"])
-
-let list = new UserList([anime])

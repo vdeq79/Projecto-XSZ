db428d2b69a0a9f022b7333967318e269968f1d9
vdeq79 <xszh79@outlook.com>
2022-11-23 19:25:30 +0100

    Completa la clase AnimePattern #19

--- src/models/anime_pattern.ts
+++ src/models/anime_pattern.ts
@@ -7,43 +7,85 @@ import { WatchingStatus } from "./watching_status";
 export class AnimePattern{
     episodePattern: number[];
     typePattern: AnimeType[];
-    /*statusPattern: Status[];
+    statusPattern: Status[];
     genresPattern: string[];
     yearPattern: number[];
-    studiosPattern: string[];*/
+    studiosPattern: string[];
 
     constructor(userList:UserList){
         var minFrequency:number = Math.floor(userList.animesUser.length/2);
-        this.episodePattern = this.searchEpisodePattern(userList.animesUser, minFrequency);
-        this.typePattern = this.searchTypePattern(userList.animesUser,minFrequency);
+        this.episodePattern = this.findEpisodePattern(userList.animesUser, minFrequency);
+        this.typePattern = this.findTypePattern(userList.animesUser,minFrequency);
+        this.statusPattern = this.findStatusPattern(userList.animesUser, minFrequency);
+        this.genresPattern = this.findGenresPattern(userList.animesUser, minFrequency);
+        this.yearPattern = this.findYearPattern(userList.animesUser,minFrequency);
+        this.studiosPattern = this.findStudiosPattern(userList.animesUser, minFrequency);
     }
 
-    searchEpisodePattern(animes:Anime[], minFrequency:number):number[]{
+    findEpisodePattern(animes:Anime[], minFrequency:number):number[]{
 
         var summary: Record<number,number> = {};
-
         animes.map(anime=>{
             summary[anime.episodes] = summary[anime.episodes]==undefined ? anime.watchingStatus:summary[anime.episodes]+anime.watchingStatus;
-        })
+        });
 
-        return this.searchPattern(summary,minFrequency,parseInt);
+        return this.findPattern(summary,minFrequency,parseInt);
     }
 
-    searchTypePattern(animes:Anime[], minFrequency:number):AnimeType[]{
+    findTypePattern(animes:Anime[], minFrequency:number):AnimeType[]{
 
         var summary: Record<AnimeType,number>={tv:0,movie:0,ova:0,special:0,ona:0,music:0};
         animes.forEach(anime=>{
             summary[anime.type]++;
         });
         
-        return this.searchPattern(summary,minFrequency);
-
+        return this.findPattern(summary,minFrequency);
     }
 
 
+    findStatusPattern(animes:Anime[], minFrequency:number):Status[]{
+        var summary: Record<Status,number> = {Airing:0, Complete:0};
+        animes.forEach(anime=>{
+            summary[anime.status]++;
+        });
+
+        return this.findPattern(summary,minFrequency);
+    }
+
+    findGenresPattern(animes:Anime[], minFrequency:number):string[]{
+        var summary: Record<string,number> = {};
+        animes.map(anime=>{
+            anime.genres.map(genre=>{
+                summary[genre] = summary[genre]==undefined ? anime.watchingStatus:summary[genre]+anime.watchingStatus;
+            })
+        });
+
+        return this.findPattern(summary,minFrequency);
+    }
+
+    findYearPattern(animes:Anime[],minFrequency:number):number[]{
+        var summary: Record<number,number> = {};
+        animes.map(anime=>{
+            summary[anime.year] = summary[anime.year]==undefined ? anime.watchingStatus:summary[anime.year]+anime.watchingStatus;
+        });
+
+        return this.findPattern(summary,minFrequency,parseInt);
+    }
+
+    findStudiosPattern(animes:Anime[], minFrequency:number):string[]{
+        var summary: Record<string,number> = {};
+        animes.map(anime=>{
+            anime.studios.map(studio=>{
+                summary[studio] = summary[studio]==undefined ? anime.watchingStatus:summary[studio]+anime.watchingStatus;
+            })
+        });
+
+        return this.findPattern(summary,minFrequency);
+    }
 
 
-    searchPattern(summary:Record<any,number>,minFrequency:number, convertFunction?:Function):any[]{
+    //Función genérica para encontrar el patrón a partir de un diccionario con las frecuencias y un valor de frecuencia mínima
+    findPattern(summary:Record<any,number>,minFrequency:number, convertFunction?:Function):any[]{
 
         var keyPattern = Object.keys(summary).filter(i=>{
             let key = convertFunction==undefined? i : convertFunction(i);
@@ -59,7 +101,3 @@ export class AnimePattern{
     }
 
 }
-
-let UL=new UserList([new Anime({default:"1"},12,AnimeType.movie,Status.airing,["shonen"],2011,["Wit"],WatchingStatus.completed),new Anime({default:"1"},12,AnimeType.movie,Status.airing,["shonen"],2011,["Wit"],WatchingStatus.completed)]);
-
-let pat = new AnimePattern(UL);

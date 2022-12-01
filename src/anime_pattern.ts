e444c58dc2c2589aa1d475dacf05aab8cf37c130
vdeq79 <xszh79@outlook.com>
2022-12-01 13:24:05 +0100

    Modifica la clase AnimePattern para tratar los elementos de forma uniforme #19

--- src/anime_pattern.ts
+++ src/anime_pattern.ts
@@ -3,65 +3,38 @@ import { Status } from "./status";
 import { UserList } from "./user_list";
 
 export class AnimePattern{
-    episodePattern: number[];
-    typePattern: AnimeType[];
-    statusPattern: Status[];
-    genresPattern: string[];
-    yearPattern: number[];
-    studiosPattern: string[];
+
+    definedPattern: string[];
 
     constructor(userList:UserList){
         //Mínimo de frecuencia admitida
         var minFrequency:number = Math.floor(userList.animesUser.length/2);
 
         //Generación de los conjuntos con un elemento con sus correspondientes frecuencias
-        var episodesSummary: Record<number,number>={};
-        var yearSummary: Record<number,number> = {};
-        var typeSummary: Record<AnimeType,number>={tv:0,movie:0,ova:0,special:0,ona:0,music:0};
-        var statusSummary: Record<Status,number> = {Airing:0, Complete:0};
-        var genresSummary: Record<string,number>={};
-        var studiosSummary: Record<string,number> = {};
+        var patternSummary: Record<(number|AnimeType|Status|string),number>={};
 
         userList.animesUser.map(anime=>{
-            episodesSummary[anime.episodes] = episodesSummary[anime.episodes]==undefined ? anime.watchingStatus:episodesSummary[anime.episodes]+anime.watchingStatus;
+            patternSummary[anime.episodes] = patternSummary[anime.episodes]==undefined ? anime.watchingStatus:patternSummary[anime.episodes]+anime.watchingStatus;
 
-            typeSummary[anime.type]++;
-            statusSummary[anime.status]++;
+            patternSummary[anime.type] = patternSummary[anime.type]==undefined ? anime.watchingStatus:patternSummary[anime.type]+anime.watchingStatus;
+
+            patternSummary[anime.status] = patternSummary[anime.status]==undefined ? anime.watchingStatus:patternSummary[anime.status]+anime.watchingStatus;;
 
             anime.genres.map(genre=>{
-                genresSummary[genre] = genresSummary[genre]==undefined ? anime.watchingStatus:genresSummary[genre]+anime.watchingStatus;
+                patternSummary[genre] = patternSummary[genre]==undefined ? anime.watchingStatus:patternSummary[genre]+anime.watchingStatus;
             })
 
-            yearSummary[anime.year] = yearSummary[anime.year]==undefined ? anime.watchingStatus:yearSummary[anime.year]+anime.watchingStatus;
+            patternSummary[anime.year] = patternSummary[anime.year]==undefined ? anime.watchingStatus:patternSummary[anime.year]+anime.watchingStatus;
 
             anime.studios.map(studio=>{
-                studiosSummary[studio] = studiosSummary[studio]==undefined ? anime.watchingStatus:studiosSummary[studio]+anime.watchingStatus;
+                patternSummary[studio] = patternSummary[studio]==undefined ? anime.watchingStatus:patternSummary[studio]+anime.watchingStatus;
             })
         });
 
-        this.episodePattern = this.extractPattern(episodesSummary, minFrequency, parseInt);
-        this.typePattern = this.extractPattern(typeSummary,minFrequency);
-        this.statusPattern = this.extractPattern(statusSummary, minFrequency);
-        this.genresPattern = this.extractPattern(genresSummary, minFrequency);
-        this.yearPattern = this.extractPattern(yearSummary,minFrequency,parseInt);
-        this.studiosPattern = this.extractPattern(studiosSummary, minFrequency);
-    }
-
-
-    //Función genérica para extraer los elementos frecuentes a partir de un diccionario con las frecuencias y un valor de frecuencia mínima
-    private extractPattern(summary:Record<any,number>, minFrequency:number, convertFunction?:Function):any[]{
-
-        var keyPattern = Object.keys(summary).filter(i=>{
-            let key = convertFunction==undefined? i : convertFunction(i);
-            return summary[key]>=minFrequency;
+        //Filtramos los conjuntos descartando los que no tienen una frecuencia mínima
+        this.definedPattern = Object.keys(patternSummary).filter(i=>{
+            return patternSummary[i]>=minFrequency;
         })
-
-
-        var convertedPattern = convertFunction==undefined? keyPattern : keyPattern.map(i=>{
-            return convertFunction(i);
-        });
-
-        return convertedPattern;
     }
 
 }
